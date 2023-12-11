ee13f7a259edc746facc989dff735155c8bc6824
Bayumbo <cybbryan@gmail.com>
2023-12-11 16:10:10 -0500

    Validaciones

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -16,12 +16,26 @@ export class RegisterComponent  implements OnInit{
     private http: HttpClient,
     private router:Router
   ) 
-{}
+{
+  
+  this.ngOnInit();
+}
+
+get userNoValido(){
+  return this.registerForm.get('username')?.invalid && this.registerForm.get('username')?.touched;
+}
+get emailNoValido(){
+  return this.registerForm.get('email')?.invalid && this.registerForm.get('email')?.touched;
+}
+
+get passwordNoValido(){
+  return this.registerForm.get('password')?.invalid && this.registerForm.get('password')?.touched;
+}
   ngOnInit(): void{
     this.registerForm=this.formBuilder.group({
-      username:'',
-      email: '',
-      password: ''
+      username:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
+      email: ['',[Validators.required,Validators.email,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(11)]],
     });
   }
 
