23bef4ccc459fdb9b191fc95103e1c87923cfeab
Bayumbo <cybbryan@gmail.com>
2023-12-11 11:39:56 -0500

    Validacion login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,15 +17,31 @@ export class LoginComponent implements OnInit{
     private router: Router
       
     
-  ){}
-
+  ){
+    this.ngOnInit();
+  }
+  get emailNoValido(){
+    return this.loginForm.get('email')?.invalid && this.loginForm.get('email')?.touched;
+  }
+  
+  get passwordNoValido(){
+    return this.loginForm.get('password')?.invalid && this.loginForm.get('password')?.touched;
+  }
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email:'',
-      password:''
+      email: ['',[Validators.required,Validators.email,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(11)]],
     });
   }
+  
   onSubmit():void{
+    console.log(this.loginForm);
+    if (this.loginForm.invalid){
+      return Object.values(this.loginForm.controls).forEach(control=>{
+
+        control.markAllAsTouched();
+      })
+    }
     this.http.post('http://localhost:3000/auth/login', this.loginForm.getRawValue(),
     {withCredentials: true}).subscribe(() => this.router.navigate(['/panel']));
   }
